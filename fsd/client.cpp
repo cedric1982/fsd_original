7dc0a37abd451dc00d6cd7ac30d0a130daef19d2
Seelenglück <113183559+cedric1982@users.noreply.github.com>
2026-01-25 16:47:48 +0100

    Update client.cpp

--- fsd/client.cpp
+++ fsd/client.cpp
@@ -1,14 +1,8 @@
 #include <cstdio>
 #include <cstdlib>
-
-
-
 #ifndef WIN32
-	#include <unistd.h>
-
+    #include <unistd.h>
 #endif
-
-
 #include <cstring>
 #include <cmath>
 
@@ -18,100 +12,6 @@
 #include "support.h"
 #include "global.h"
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 client *rootclient=NULL;
 
 client::client(char *i, server *where, char *cs, int t, int reqrating,
@@ -120,22 +20,30 @@ client::client(char *i, server *where, char *cs, int t, int reqrating,
    next=rootclient, prev=NULL;
    if (next) next->prev=this;
    rootclient=this;
+
    location=where, cid=strdup(i), type=t, callsign=strdup(cs);
    protocol=strdup(rev), sector=NULL, identflag=NULL, facilitytype=0;
    rating=reqrating, visualrange=40;
    plan=NULL, positionok=0, altitude=0, simtype=st;
    realname=strdup(real), starttime=alive=mtime();
    frequency=0; transponder=0; groundspeed=0; lat=0; lon=0;
+
+   // --------------------------------------------------------------------
+   // Swift / CQ state cache + debounce state (needed by cluser.cpp CQ logic)
+   // --------------------------------------------------------------------
    has_cq = 0;
-   cq_ts = 0;
-   last_cq[0] = '\0';
-   gear_known = 0;
-   gear_down_last = 0;
-   gear_last_change = 0;
+   cq_ts  = 0;
+   if (sizeof(last_cq) > 0) last_cq[0] = '\0';
+
    ground_known = 0;
    on_ground_last = 0;
    ground_last_change = 0;
+
+   gear_known = 0;
+   gear_down_last = 0;
+   gear_last_change = 0;
 }
+
 client::~client()
 {
    serverinterface->clientdropped(this);
@@ -149,6 +57,7 @@ client::~client()
    if (sector) free(sector);
    if (identflag) free(identflag);
 }
+
 void client::handlefp(char **array)
 {
    int revision=plan?plan->revision+1:0;
@@ -160,35 +69,31 @@ void client::handlefp(char **array)
       array[13], array[14]);
    plan->revision=revision;
 }
+
 /* Update the client fields, given a packet array */
 void client::updatepilot(char **array)
 {
    unsigned a,b,c;
    unsigned x,y,z;
+
    transponder=atoi(array[2]);
    if (identflag) free(identflag);
    identflag=strdup(array[0]);
+
    sscanf(array[4],"%lf",&lat);
    sscanf(array[5],"%lf",&lon);
-if (lat>90.0||lat<-90.0||lon>180.0||lon<-180.0) dolog(L_DEBUG, "POSERR: s=(%s,%s) got(%f,%f)", array[4], array[5], lat, lon);
+   if (lat>90.0||lat<-90.0||lon>180.0||lon<-180.0)
+      dolog(L_DEBUG, "POSERR: s=(%s,%s) got(%f,%f)", array[4], array[5], lat, lon);
 
    altitude=atoi(array[6]);
-//if (altitude > 100000 || altitude < 0)
-//	altitude=0;
    groundspeed=atoi(array[7]);
    pbh=(unsigned int)strtoul(array[8],(char **)NULL,10);
-
-//x=(pbh&4290772992)>>22;
-//y=(pbh&4190208)>>12;
-//z=(pbh&4092)>>2;
-//dolog(L_INFO,"PBH value in text is %s", array[8]);
-//dolog(L_INFO,"PBH unsigned value is %u",pbh);
-//dolog(L_INFO,"P=%u B=%u H=%u",x,y,z);
 
    flags=atoi(array[9]);
    setalive();
    positionok=1;
 }
+
 void client::updateatc(char **array)
 {
    int newfreq=atoi(array[0]);
@@ -201,274 +106,32 @@ void client::updateatc(char **array)
    setalive();
    positionok=1;
 }
+
 client *getclient(char *ident)
 {
    client *temp;
-
-
    for (temp=rootclient;temp;temp=temp->next)
       if (!STRCASECMP(ident,temp->callsign))
       return temp;
    return NULL;
 }
-void client::setalive()
 
+void client::setalive()
 {
    alive=mtime();
-
-
-
-
-
 }
-double client::distance(client *other)
 
+double client::distance(client *other)
 {
    if (!other) return -1;
    if (!positionok||!other->positionok) return -1;
    return dist(lat,lon,other->lat,other->lon);
    return 1;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
-int client::getrange()
 
+int client::getrange()
 {
    if (type==CLIENT_PILOT)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
    { 
       if (altitude<0) altitude=0;
       return (int) (10+1.414*sqrt((double)altitude));
@@ -476,7 +139,7 @@ int client::getrange()
    switch (facilitytype)
    {
       case 0: return 40;       /* Unknown */
-      case 1: return 1500;      /* FSS */
+      case 1: return 1500;     /* FSS */
       case 2: return 5;        /* CLR_DEL */
       case 3: return 5;        /* GROUND */
       case 4: return 30;       /* TOWER  */
@@ -502,7 +165,7 @@ flightplan::flightplan(char *cs, char itype, char *iaircraft, int
    alt=strdup(ialt);
    destairport=strdup(idestairport);
    hrsenroute=ihrsenroute;
-   minenroute=iminenroute;
+   minenroute=imenroute;
    hrsfuel=ihrsfuel;
    minfuel=iminfuel;
    altairport=strdup(ialtairport);
@@ -520,486 +183,4 @@ flightplan::~flightplan()
    if (altairport) free(altairport);
    if (remarks) free(remarks);
    if (route) free(route);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 }
